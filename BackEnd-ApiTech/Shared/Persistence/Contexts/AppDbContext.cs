55b83a059d1f27a4e4bea799fc2e6924e40f415d
Mirroy25 <112901601+Mirroy25@users.noreply.github.com>
2023-11-05 02:17:47 -0600

    feat(task)= aggregate context.

---
+++ BackEnd-ApiTech/Shared/Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using BackEnd_ApiTech.security.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd_ApiTech.Shared.Persistence.Contexts;
+
+public class AppDbContext: DbContext
+{
+    public DbSet<User> Users { get; set; }
+    
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        // Users
+            
+        // Constraints
+        builder.Entity<User>().ToTable("Users");
+        builder.Entity<User>().HasKey(p => p.Id);
+        builder.Entity<User>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<User>().Property(p => p.FullName).IsRequired().HasMaxLength(30);
+        builder.Entity<User>().Property(p => p.Email).IsRequired();
+        builder.Entity<User>().Property(p => p.Birthday).IsRequired();
+        builder.Entity<User>().Property(p => p.Password).IsRequired();
+        builder.Entity<User>().Property(p => p.ConfirmPassword).IsRequired();
+        builder.Entity<User>().Property(p => p.Role).IsRequired();
+      
+    }
+}
