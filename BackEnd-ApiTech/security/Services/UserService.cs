e367667f86cee166e9b8994af27999ad0882cca0
Mirroy25 <112901601+Mirroy25@users.noreply.github.com>
2023-11-05 03:09:53 -0600

    feat(task): aggregate user service.

---
+++ BackEnd-ApiTech/security/Services/UserService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using BackEnd_ApiTech.security.Domain.Models;
+using BackEnd_ApiTech.security.Domain.Repositories;
+using BackEnd_ApiTech.security.Domain.Services.Communication;
+using BackEnd_ApiTech.security.Services.Communication;
+
+namespace BackEnd_ApiTech.security.Services;
+
+public class UserService: IUserService
+{
+    private readonly IUserRepository _userRepository;
+   
+    private readonly IMapper _mapper;
+
+    public UserService(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public Task<AuthenticateResponse> Authenticate(AuthenticateRequest model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<User>> ListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RegisterAsync(RegisterRequest model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(int id, UpdateRequest model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
