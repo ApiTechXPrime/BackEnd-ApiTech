032f918816f9f061448198fc047b1e2f57dcebc8
Mirroy25 <112901601+Mirroy25@users.noreply.github.com>
2023-11-05 01:05:59 -0600

    feat(task): aggregate service interface of user

---
+++ BackEnd-ApiTech/security/Domain/Services/Communication/IUserService.cs
@@ -0,0 +1,14 @@
+using BackEnd_ApiTech.security.Domain.Models;
+using BackEnd_ApiTech.security.Services.Communication;
+
+namespace BackEnd_ApiTech.security.Domain.Services.Communication;
+
+public interface IUserService
+{
+    Task<AuthenticateResponse> Authenticate(AuthenticateRequest model);
+    Task<IEnumerable<User>> ListAsync();
+    Task<User> GetByIdAsync(int id);
+    Task RegisterAsync(RegisterRequest model);
+    Task UpdateAsync(int id, UpdateRequest model);
+    Task DeleteAsync(int id);
+}
