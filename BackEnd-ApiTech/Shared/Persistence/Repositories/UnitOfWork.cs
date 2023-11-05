e9df0c76c0c81b806b81e20e177a532d931a3908
LeonardoFerreyraC <u202111363@upc.edu.pe>
2023-11-05 07:03:24 -0500

    feat(analytics) Implementando la clase UnitOfWork

---
+++ BackEnd-ApiTech/Shared/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using BackEnd_ApiTech.Shared.Persistence.Contexts;
+
+namespace BackEnd_ApiTech.Shared.Persistence.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
