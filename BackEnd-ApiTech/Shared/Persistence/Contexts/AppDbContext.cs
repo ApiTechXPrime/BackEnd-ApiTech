9e2ceeb35555132c8c6c3e728a3befcae0d24260
LeonardoFerreyraC <u202111363@upc.edu.pe>
2023-11-05 06:46:33 -0500

    feat(analytics) Agregando AppDbContext

---
+++ BackEnd-ApiTech/Shared/Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using BackEnd_ApiTech.Shared.Extensions;
+using BackEnd_ApiTech.TechXPrime.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd_ApiTech.Shared.Persistence.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<Analytic>().ToTable("Analytics");
+        builder.Entity<Analytic>().HasKey(p => p.Id);
+        builder.Entity<Analytic>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Analytic>().Property(p => p.Week).IsRequired();
+        builder.Entity<Analytic>().Property(p => p.Month).IsRequired();
+        builder.Entity<Analytic>().Property(p => p.Year).IsRequired();
+        builder.Entity<Analytic>().ToTable(t => t.HasCheckConstraint("CK_Analytics_Week", "Week >= 1 AND Week <= 4"));
+        builder.Entity<Analytic>().ToTable(t => t.HasCheckConstraint("CK_Analytics_Month", "Month >= 1 AND Month <= 12"));
+        
+        
+        builder.UseSnakeCaseNamingConvention();
+    }
+
+
+    public DbSet<Analytic> Analytics { get; set; }
+}
