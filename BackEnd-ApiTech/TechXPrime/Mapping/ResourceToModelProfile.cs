5f25370277e4940a500d246a8687ae94aba2553b
LeonardoFerreyraC <u202111363@upc.edu.pe>
2023-11-05 05:37:58 -0500

    feat(analytic) Agregando mapeo de resource y model

---
+++ BackEnd-ApiTech/TechXPrime/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using BackEnd_ApiTech.TechXPrime.Domain.Models;
+using BackEnd_ApiTech.TechXPrime.Resources;
+
+namespace BackEnd_ApiTech.TechXPrime.Mapping;
+
+public class ResourceToModelProfile : Profile
+{
+    public ResourceToModelProfile()
+    {
+        CreateMap<SaveAnalyticResource, Analytic>();
+    }
+}
