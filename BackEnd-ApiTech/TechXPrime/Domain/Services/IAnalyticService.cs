ba1748d77656553f92478a74f25b10f1bbf59fff
LeonardoFerreyraC <u202111363@upc.edu.pe>
2023-11-05 05:23:49 -0500

    feat(analytic) Agregando interfaz del servicio analytic

---
+++ BackEnd-ApiTech/TechXPrime/Domain/Services/IAnalyticService.cs
@@ -0,0 +1,12 @@
+﻿using BackEnd_ApiTech.TechXPrime.Domain.Models;
+using BackEnd_ApiTech.TechXPrime.Domain.Services.Communication;
+
+namespace BackEnd_ApiTech.TechXPrime.Domain.Services;
+
+public interface IAnalyticService
+{
+    Task<IEnumerable<Analytic>> ListByMonthAsync(int month);
+    Task<AnalyticResponse> SaveAsync(Analytic analytic);
+    Task<AnalyticResponse> UpdateAsync(int id, Analytic analytic);
+    Task<AnalyticResponse> DeleteAsync(int id);
+}
