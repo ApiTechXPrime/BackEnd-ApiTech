71466c01c882bc52a00f3092b27b0a90276b10d9
LeonardoFerreyraC <u202111363@upc.edu.pe>
2023-11-05 07:03:43 -0500

    feat(analytics) Implementando el repositorio analytics

---
+++ BackEnd-ApiTech/TechXPrime/Persistence/Repositories/AnalyticsRepository.cs
@@ -0,0 +1,46 @@
+﻿using BackEnd_ApiTech.Shared.Persistence.Contexts;
+using BackEnd_ApiTech.Shared.Persistence.Repositories;
+using BackEnd_ApiTech.TechXPrime.Domain.Models;
+using BackEnd_ApiTech.TechXPrime.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd_ApiTech.TechXPrime.Persistence.Repositories;
+
+public class AnalyticsRepository : BaseRepository, IAnalyticRepository
+{
+    public AnalyticsRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Analytic>> ListAsync()
+    {
+        return await _context.Analytics.ToListAsync();
+    }
+
+    public async Task AddAsync(Analytic analytic)
+    {
+        await _context.Analytics.AddAsync(analytic);
+    }
+
+    public async Task<Analytic> FindByIdAsync(int id)
+    {
+        return await _context.Analytics.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Analytic>> FindByMonthAsync(int month)
+    {
+        return await _context.Analytics
+            .Where(p => p.Month == month)
+            .ToListAsync();
+    }
+
+    public void Update(Analytic analytic)
+    {
+        _context.Analytics.Update(analytic);
+    }
+
+    public void Remove(Analytic analytic)
+    {
+        _context.Analytics.Remove(analytic);
+    }
+}
