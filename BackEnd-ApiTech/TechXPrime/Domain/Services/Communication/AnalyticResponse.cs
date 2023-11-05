5bc7a0b9b01f1602853cd38bd34424f17b31b594
LeonardoFerreyraC <u202111363@upc.edu.pe>
2023-11-05 05:23:28 -0500

    feat(analytic) Agregando respuesta de analytic

---
+++ BackEnd-ApiTech/TechXPrime/Domain/Services/Communication/AnalyticResponse.cs
@@ -0,0 +1,15 @@
+﻿using BackEnd_ApiTech.Shared.Domain.Services.Communication;
+using BackEnd_ApiTech.TechXPrime.Domain.Models;
+
+namespace BackEnd_ApiTech.TechXPrime.Domain.Services.Communication;
+
+public class AnalyticResponse : BaseResponse<Analytic>
+{
+    public AnalyticResponse(string message) : base(message)
+    {
+    }
+
+    public AnalyticResponse(Analytic resource) : base(resource)
+    {
+    }
+}
