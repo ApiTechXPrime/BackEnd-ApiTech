5f9d81e1f5df86a59f94aba136e27a8f65bcb0d5
LeonardoFerreyraC <u202111363@upc.edu.pe>
2023-11-25 08:43:14 -0500

    chore(main) Changing to string

--- BackEnd-ApiTech/TechXPrime/Resources/OrderResource.cs
+++ BackEnd-ApiTech/TechXPrime/Resources/OrderResource.cs
@@ -9,7 +9,7 @@ public class OrderResource
     public string Problem { get; set; }
     public string ComponentsToUse { get; set; }
     public int ValueProgress { get; set; }
-    public DateTime DeliveryDay { get; set; }
+    public string DeliveryDay { get; set; }
     public float Income { get; set; }
     public int Finished { get; set; }
     public float Investment { get; set; }
